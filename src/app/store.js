72ceb0128d03fc6ec74342a9579fb5636db9ab5d
fabian-gl <fabianliuni@gmail.com>
2021-10-27 17:04:54 -0300

    Changed store to save and load whole state instead of just the token

--- src/app/store.js
+++ src/app/store.js
@@ -1,27 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from '../features/user/userSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../features/user/userSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
   },
-})
+  preloadedState: JSON.parse(localStorage.getItem("redux-state")) || {},
+});
 
-let currentAuthState
+let currentAuthState;
 // In this function we handle the side effects of setting and deleting the local storage
 function handleChange() {
-  const state = store.getState()
+  const state = store.getState();
 
-  const previousAuthState = currentAuthState
-  currentAuthState = state.user.authenticated
+  const previousAuthState = currentAuthState;
+  currentAuthState = state.user.authenticated;
 
   // We will only act if the authenticated property changed
   if (previousAuthState !== currentAuthState) {
-    if (currentAuthState === false) localStorage.removeItem('token')
-    else localStorage.setItem('token', state.user.token)
+    if (currentAuthState === false) localStorage.removeItem("redux-state");
+    else localStorage.setItem("redux-state", JSON.stringify(state));
   }
 }
 
-store.subscribe(handleChange)
+store.subscribe(handleChange);
 
-export default store
+export default store;
