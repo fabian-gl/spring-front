ae3c7ef690dd3f62f196a1ebb42bbc19a13898bf
fabian-gl <fabianliuni@gmail.com>
2021-10-28 01:53:52 -0300

    Got rid of mock Home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Home from "../components/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import Posts from "../components/Posts";
@@ -14,7 +13,6 @@ export default function Router() {
     <BrowserRouter>
       <Header />
       <Switch>
-        <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/posts" component={Posts} />
