38e35af6ec509814a76fe425ad9fb999af8b32d6
fabian-gl <fabianliuni@gmail.com>
2021-10-27 22:32:28 -0300

    Added dependency for pagination and added component to Photos

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 
-import { getPhotos } from "../utils/serverCalls";
+import ReactPaginate from "react-paginate";
+import "./paginator.css";
 
+import { getPhotos } from "../utils/serverCalls";
 import PhotoItem from "./PhotoItem";
+
+import NothingToShow from "./NothingToShow";
 
 import {
   Flex,
@@ -12,42 +16,78 @@ import {
   SimpleGrid,
 } from "@chakra-ui/react";
 
-const Photos = () => {
-  const [photos, setPhotos] = useState([]);
-
-  const getAllPhotos = async () => {
-    const fetchedPhotos = await getPhotos();
-    setPhotos(fetchedPhotos);
-  };
-  useEffect(() => {
-    getAllPhotos();
-  }, []);
-
+const PhotosSlice = ({ photosSlice }) => {
+  const colorMode = useColorModeValue("gray.50", "gray.800");
   return (
-    <Flex
-      minH="85vh"
-      align="center"
-      justify="center"
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
+    <Flex minH="85vh" align="center" justify="center" bg={colorMode}>
       <Stack spacing={8} mx="auto" maxW="lg" py={12} px={6} minW="80vw">
         <Stack align="center">
           <Heading fontSize="4xl">Photos</Heading>
         </Stack>
-        <SimpleGrid
-          columns={[1, 2, 3, 4, 5]}
-          rounded="lg"
-          bg={useColorModeValue("white", "gray.700")}
-          boxShadow="lg"
-          py={8}
-          px={[0, 0, 8]}
-        >
-          {photos.map(({ id, title, thumbnailUrl }) => (
-            <PhotoItem key={id} title={title} thumbnailUrl={thumbnailUrl} />
-          ))}
-        </SimpleGrid>
+
+        {photosSlice.length ? (
+          <SimpleGrid
+            columns={[1, 2, 3, 4, 5]}
+            rounded="lg"
+            bg={colorMode}
+            boxShadow="lg"
+            py={8}
+            px={[0, 0, 8]}
+          >
+            {photosSlice.map(({ id, title, thumbnailUrl }) => (
+              <PhotoItem key={id} title={title} thumbnailUrl={thumbnailUrl} />
+            ))}
+          </SimpleGrid>
+        ) : (
+          <NothingToShow message="No pictures to show" />
+        )}
       </Stack>
     </Flex>
+  );
+};
+
+const Photos = () => {
+  const [photos, setPhotos] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+
+  const limit = 10;
+
+  useEffect(() => {
+    const getSomePhotos = async () => {
+      const response = await getPhotos(limit, offset);
+      setTotalCount(response.count);
+      setPhotos(response.photos);
+      setPageCount(Math.ceil(response.count / limit));
+    };
+
+    // getSomePhotos();
+  }, [offset]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * limit) % totalCount;
+    setOffset(newOffset);
+  };
+
+  return (
+    <>
+      <PhotosSlice photosSlice={photos} />
+      <Flex>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          containerClassName={"pagination"}
+          activeClassName={"active"}
+        />
+      </Flex>
+    </>
   );
 };
 
